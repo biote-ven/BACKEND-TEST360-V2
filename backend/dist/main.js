0780be712275f3b8d3e17f731fa9d22075e2b42b
Biote <biote.ven@gmail.com>
2025-07-21 23:31:38 -0400

    Actualización Conf Build Proyecto

--- backend/dist/main.js
+++ backend/dist/main.js
@@ -5,6 +5,12 @@ const app_module_1 = require("./app.module");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     await app.listen(process.env.PORT ?? 3000);
+    if (process.env.VERCEL) {
+        return app.getHttpAdapter().getInstance();
+    }
+    else {
+        await app.listen(3000);
+    }
 }
-module.exports = bootstrap().then(app => app.getHttpAdapter().getInstance());
+module.exports = bootstrap();
 //# sourceMappingURL=main.js.map
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,8 +4,18 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
+
+   // Exportación para Vercel
+  if (process.env.VERCEL) {
+    return app.getHttpAdapter().getInstance();
+  } else {
+    await app.listen(3000);
+  }
 }
+
+// Export para Vercel
+module.exports = bootstrap();
 
 //bootstrap();
 // Export para Vercel
-module.exports = bootstrap().then(app => app.getHttpAdapter().getInstance());
+//module.exports = bootstrap().then(app => app.getHttpAdapter().getInstance());
